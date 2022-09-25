ad61db893beceaab88f41d27a999c1d0e1d15fa7
John Canero <johncanero.official@gmail.com>
2022-09-25 19:17:15 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+      
+      },
+   
+      fontSize: {
+  
+      },
+
+      spacing: {
+
+      }
+    },
+
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
