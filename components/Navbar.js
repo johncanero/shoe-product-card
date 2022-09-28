c42960f73f8bc33c9f3c48ce0600cabfb38e17ee
John Canero <johncanero.official@gmail.com>
2022-09-28 17:04:20 +0800

    Modify Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,17 +20,19 @@ const Navbar = () => {
           </Link>
           <div className="flex md:order-2">
           
+            {/* Search Button - Mobile */}
             <button
-              type="button"
-              data-collapse-toggle="navbar-search"
-              aria-controls="navbar-search"
-              aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
-            >
-            
-              <span className="sr-only">Search</span>
+                type="button"
+                data-collapse-toggle="navbar-search"
+                aria-controls="navbar-search"
+                aria-expanded="false"
+                className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100rounded-lg text-sm p-2.5 mr-1"
+              >
+                  <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                <span className="sr-only">Search</span>
             </button>
 
+            {/* Search Icon with Input */}
             <div className="hidden relative md:block">
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg
@@ -49,13 +51,15 @@ const Navbar = () => {
                 <span className="sr-only">Search icon</span>
               </div>
 
+              {/* Input Field - Tablet and Desktop */}
               <input
                 type="text"
                 id="search-navbar"
-                className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block p-2 pl-10 w-full text-gray-900 bg-gray-100 rounded-2xl sm:text-sm dark:placeholder-gray-400 dark:text-black"
                 placeholder="Search..."
               />
             </div>
+
             <button
               data-collapse-toggle="navbar-search"
               type="button"
