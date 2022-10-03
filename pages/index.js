9885893b4f882e6b9a369f18b85b5c72cfc55008
John Canero <johncanero.official@gmail.com>
2022-10-03 22:46:53 +0800

    Nike Favicon Logo

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           <meta itemprop="name" content="Shoe Card" />
           <meta itemprop="description" content="Shoe Product Card created with Next JS and Tailwind CSS" />
           <meta itemprop="image" content="Shoe Card Feature" />
-          <link rel="icon" href="/images/nikeLogo.png" />     
+          <link rel="icon" href="/images/nikeLogoBlack.png" />     
       </Head>
 
       <main>
