510bfb376ad677c701481cb70f93cba14f2c3d9a
John Canero <johncanero.official@gmail.com>
2022-10-03 22:47:45 +0800

    Text Color of the Footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer
-        className="text-gray-700 text-center font-Poppins sm:bg-white md:bg-transparent p-4 md:bottom-0 md:fixed md:inset-x-0"
+        className="text-black text-center font-Poppins sm:bg-white md:bg-transparent p-4 md:bottom-0 md:fixed md:inset-x-0"
       >
         © 2022 Copyright:
         <span className="font-semibold px-1">John Cañero</span>
